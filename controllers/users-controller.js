b6c4aa71bf2da4c7bf54c1b1f5a81bcc356d4fdd
Daniel Risdon <drisdon07@gmail.com>
2017-10-04 09:16:43 -0400

    basic file structure

---
+++ controllers/users-controller.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+const router = require('express').Router();
+const passport = require('passport');
+
+const auth = require('../services/auth');
+
+// Sign up page.
+
+router.get('/new', (req, res) => {
+  res.render('users/new');
+});
+
+// Post to create new user (params are username/password).
+
+router.post('/',
+  passport.authenticate(
+    'local-signup', {
+        failureRedirect: 'users/new',
+        successRedirect: '/messages'
+    }
+  )
+);
+
+// Post to login (params are username/password).
+
+router.post('/login',
+  passport.authenticate(
+    'local-login', {
+        failureRedirect: '/',
+        successRedirect: '/messages'
+    }
+));
+
+// Logout.
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;

64331d072f5d1b307c42dd7cc7efd57d953a1109
Daniel Risdon <drisdon07@gmail.com>
2017-10-10 15:19:38 -0400

    fixed sign up bug

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,10 @@
 // passport
 const passport = require('passport');
-// We're going to need the User model
+// User model
 const User = require('../models/user');
-// And we're going to need the Local Strategy for this kind of registration
+// local strategy
 const LocalStrategy = require('passport-local').Strategy;
-// We'll also need bcrypt to authenticate uses without storing their
-// passoword _anywhere_...
+// bcrypt
 const bcrypt = require('bcryptjs');
 
 const authObject = {};
